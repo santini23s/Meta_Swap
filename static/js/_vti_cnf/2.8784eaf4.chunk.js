bed4db618af57b5b75491b758f82c8bcb02cfbaf
santini23s <81830167+santini23s@users.noreply.github.com>
2022-01-24 16:08:21 -0500

    Add files via upload

---
+++ static/js/_vti_cnf/2.8784eaf4.chunk.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|13 Jan 2022 21:10:17 -0000
+vti_author:SR|DESKTOP-JU3LBMG\\Santini
+vti_modifiedby:SR|DESKTOP-JU3LBMG\\Santini
+vti_nexttolasttimemodified:TW|13 Jan 2022 21:10:17 -0000
+vti_timecreated:TR|24 Jan 2022 21:00:19 -0000
+vti_cacheddtm:TX|24 Jan 2022 21:00:19 -0000
+vti_filesize:IR|1269080
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
---
+++ static/js/_vti_cnf/main.114d694d.chunk.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|21 Jan 2022 22:57:35 -0000
+vti_author:SR|DESKTOP-JU3LBMG\\Santini
+vti_modifiedby:SR|DESKTOP-JU3LBMG\\Santini
+vti_nexttolasttimemodified:TW|21 Jan 2022 22:57:35 -0000
+vti_timecreated:TR|24 Jan 2022 21:00:19 -0000
+vti_cacheddtm:TX|24 Jan 2022 21:00:19 -0000
+vti_filesize:IR|22997
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
